244ce0f8d54e3bb495fa123ceb6187bd425bfef3
JuanmaDevG <juanmagonzalezmartinez@protonmail.com>
2025-11-06 15:54:06 +0100

    Starting the next problem

---
+++ challenges/truck-fleet.py
@@ -0,0 +1,28 @@
+import numba
+import numpy
+
+
+"""
+    TODO: describe the truck fleet problem
+"""
+
+
+class P010:
+    def __init__(self):
+        self.T = 700
+
+
+    def pdr(self) -> [int]:
+        return None
+
+
+    def pdr_a(self) -> [int]:
+        return None
+
+
+    def pdi(self) -> [int]:
+        return None
+
+
+if __name__ == "__main__":
+    #TODO: code here
